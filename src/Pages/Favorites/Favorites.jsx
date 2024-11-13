a8296934babd59751c87ad80622f334109a38836
LeozinGs <lokinho_ddtank@hotmail.com>
2024-11-12 23:01:26 -0300

    Deixa a página de favoritos mais bonita.

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -1,10 +1,15 @@
 import './styles.css';
+import magikarpIcon from './assets/magikarp-background.svg';
 
 const Favorites = () => {
     return (
-        <>
-            <h1>Favorites</h1>
-        </>
+        <div className='favorites-container'>
+            <div className="no-favorites">
+                <img className='favorites-image--background' src={magikarpIcon} alt="magikarp background" />
+                <h4 className='favorite-text--description'>{'You didn`t favorite any Pokémon :('}</h4>
+                <p className="favorite-text--hint">Click on the heart icon of your <br />favorite Pokémon and they will appear here.</p>
+            </div>
+        </div>
     );
 }
 
