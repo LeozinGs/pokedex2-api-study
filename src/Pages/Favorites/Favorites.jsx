7c04b3093da6fa4301a41c810ae8bcf0e6a994bf
LeozinGs <lokinho_ddtank@hotmail.com>
2024-11-19 02:02:54 -0300

    Cria método para filtrar pokémons por tipo.

--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -44,6 +44,7 @@ const Favorites = () => {
 
     return (
         <div className='favorite-page'>
+            <h3 className="favorites-title">Favorites</h3>
             <div className="cards--container">
                 {pokemonData && pokemonData.length > 0 ? (
                     pokemonData.map((pokemon) => (
@@ -57,12 +58,10 @@ const Favorites = () => {
                         />
                     ))
                 ) : (
-                    <div className='favorites-container'>
-                        <div className="no-favorites">
-                            <img className='favorites-image--background' src={magikarpIcon} alt="magikarp background" />
-                            <h4 className='favorite-text--description'>{'You didn`t favorite any Pokémon :('}</h4>
-                            <p className="favorite-text--hint">Click on the heart icon of your <br />favorite Pokémon and they will appear here.</p>
-                        </div>
+                    <div className="no-favorites">
+                        <img className='favorites-image--background' src={magikarpIcon} alt="magikarp background" />
+                        <h4 className='favorite-text--description'>{'You didn`t favorite any Pokémon :('}</h4>
+                        <p className="favorite-text--hint">Click on the heart icon of your <br />favorite Pokémon and they will appear here.</p>
                     </div>
                 )}
             </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,143 +1,3 @@
-// import { useEffect, useState } from 'react';
-// import SearchBar from '../../Components/SearchBar/SearchBar';
-// import './styles.css';
-// import Axios from 'axios';
-// import { IMAGE_API_URL, POKEMON_API_URL } from '../../Config';
-// import Loading from '../../Components/Loading/Loading';
-// import Card from '../../Components/Card/Card';
-
-// const Home = () => {
-//     const [pokemonData, setPokemonData] = useState(null);
-//     const [filteredPokemonData, setFilteredPokemonData] = useState(null);
-//     const [visiblePokemonCount, setVisiblePokemonCount] = useState(10); // Mostra 10 Pokémon inicialmente
-//     const [loadingMore, setLoadingMore] = useState(false);
-//     const [favorites, setFavorites] = useState(() => {
-//         return JSON.parse(localStorage.getItem("favorites")) || [];
-//     });
-
-//     useEffect(() => {
-//         Axios.get(POKEMON_API_URL + `?limit=1025`)
-//             .then((res) => {
-//                 if (res.status >= 200 && res.status < 300) {
-//                     const { results } = res.data;
-//                     let newPokemonData = [];
-
-//                     results.forEach((pokemon, index) => {
-//                         index++;
-//                         let pokemonObject = {
-//                             id: index,
-//                             url: IMAGE_API_URL + index + '.png',
-//                             name: pokemon.name,
-//                         };
-//                         newPokemonData.push(pokemonObject);
-//                     });
-//                     setPokemonData(newPokemonData);
-//                     setFilteredPokemonData(newPokemonData);  // Inicializa com todos os dados
-//                 }
-//             });
-//     }, []);
-
-//     const handleSearch = async (query) => {
-//         // Se o input estiver vazio, restaura os dados originais
-//         if (!query) {
-//             setFilteredPokemonData(pokemonData); // Volta a mostrar todos os Pokémon
-//             return;
-//         }
-
-//         const isNumeric = !isNaN(query);
-//         if (isNumeric) {
-//             // Busca por ID
-//             const pokemonById = pokemonData.find(pokemon => pokemon.id === parseInt(query));
-//             setFilteredPokemonData(pokemonById ? [pokemonById] : []);
-//         } else {
-//             // Busca por nome ou tipo
-//             const pokemonByName = pokemonData.filter(pokemon =>
-//                 pokemon.name.toLowerCase().includes(query.toLowerCase())
-//             );
-
-//             if (pokemonByName.length === 0) {
-//                 try {
-//                     const typeRes = await Axios.get(`${POKEMON_API_URL}/type/${query.toLowerCase()}`);
-//                     const pokemonByType = typeRes.data.pokemon.map(p => p.pokemon.name);
-//                     const pokemonByTypeData = pokemonData.filter(pokemon => pokemonByType.includes(pokemon.name));
-//                     setFilteredPokemonData(pokemonByTypeData);
-//                 } catch (error) {
-//                     console.log(error, "Tipo de Pokémon não encontrado");
-//                     setFilteredPokemonData([]);
-//                 }
-//             } else {
-//                 setFilteredPokemonData(pokemonByName);
-//             }
-//         }
-//     };
-
-//     const loadMorePokemon = () => {
-//         if (loadingMore) return;
-//         setLoadingMore(true);
-//         setVisiblePokemonCount((prevCount) => prevCount + 10);
-//         setLoadingMore(false);
-//     };
-
-//     useEffect(() => {
-//         const handleScroll = () => {
-//             const cardsContainer = document.querySelector('.cards--container');
-//             if (!cardsContainer) return;
-
-//             const lastVisibleCard = cardsContainer.children[visiblePokemonCount - 1];
-//             if (lastVisibleCard) {
-//                 const lastCardOffset = lastVisibleCard.getBoundingClientRect().bottom;
-//                 const pageOffset = window.innerHeight + window.scrollY;
-
-//                 // Carrega mais Pokémon quando a posição do último card visível está perto do final da tela
-//                 if (pageOffset >= lastCardOffset - 200) {
-//                     loadMorePokemon();
-//                 }
-//             }
-//         };
-
-//         window.addEventListener('scroll', handleScroll);
-//         return () => window.removeEventListener('scroll', handleScroll);
-//         // eslint-disable-next-line react-hooks/exhaustive-deps
-//     }, [loadingMore, visiblePokemonCount]);
-
-//     useEffect(() => {
-//         localStorage.setItem("favorites", JSON.stringify(favorites));
-//     }, [favorites]);
-
-//     const toggleFavorite = (id) => {
-//         setFavorites((prevFavorites) =>
-//             prevFavorites.includes(id)
-//                 ? prevFavorites.filter(favId => favId !== id)
-//                 : [...prevFavorites, id]
-//         );
-//     };
-
-//     return (
-//         <main className='container'>
-//             <SearchBar onSearch={handleSearch} />
-//             <div className="cards--container">
-//                 {filteredPokemonData ? (
-//                     filteredPokemonData.slice(0, visiblePokemonCount).map((pokemon) => (
-//                         <Card
-//                             key={pokemon.id}
-//                             id={pokemon.id}
-//                             image={pokemon.url}
-//                             name={pokemon.name}
-//                             isFavorite={favorites.includes(pokemon.id)}
-//                             onToggleFavorite={() => toggleFavorite(pokemon.id)}
-//                         />
-//                     ))
-//                 ) : (
-//                     <Loading size={65} color={'var(--clr-dark-blue)'} />
-//                 )}
-//                 {loadingMore && <Loading size={40} color={'var(--clr-dark-blue)'} />}
-//             </div>
-//         </main>
-//     );
-// }
-
-// export default Home;
-
 import { useEffect, useState, useRef } from 'react';
 import SearchBar from '../../Components/SearchBar/SearchBar';
 import './styles.css';
@@ -149,27 +9,56 @@ import Card from '../../Components/Card/Card';
 const Home = () => {
     const [pokemonData, setPokemonData] = useState(null);
     const [filteredPokemonData, setFilteredPokemonData] = useState(null);
-    const [visiblePokemon, setVisiblePokemon] = useState([]); // Índices dos Pokémon visíveis
+    const [visiblePokemon, setVisiblePokemon] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [types, setTypes] = useState([]);
+    const [selectedType, setSelectedType] = useState(
+        () => localStorage.getItem('selectedType') || '' // Restaura do localStorage
+    );
     const observerRef = useRef(null);
+
+    const fetchPokemonDetails = async (results) => {
+        const promises = results.map((pokemon) =>
+            Axios.get(pokemon.url).then((res) => {
+                const { id, types } = res.data;
+                return {
+                    id,
+                    name: pokemon.name,
+                    url: IMAGE_API_URL + id + '.png',
+                    sprite: `https://img.pokemondb.net/artwork/vector/${pokemon.name}.png`,
+                    types: types.map((typeInfo) => typeInfo.type.name),
+                };
+            })
+        );
+
+        return Promise.all(promises);
+    };
 
     useEffect(() => {
         Axios.get(POKEMON_API_URL + `?limit=1025`)
             .then((res) => {
                 if (res.status >= 200 && res.status < 300) {
                     const { results } = res.data;
-                    const newPokemonData = results.map((pokemon, index) => ({
-                        id: index + 1,
-                        url: IMAGE_API_URL + (index + 1) + '.png',
-                        name: pokemon.name,
-                        sprite: `https://img.pokemondb.net/artwork/vector/${pokemon.name}.png`,
-                    }));
-                    setPokemonData(newPokemonData);
-                    setFilteredPokemonData(newPokemonData);
-                    setLoading(false);
+
+                    fetchPokemonDetails(results).then((detailedPokemonData) => {
+                        setPokemonData(detailedPokemonData);
+                        setFilteredPokemonData(detailedPokemonData);
+
+                        const allTypes = new Set(
+                            detailedPokemonData.flatMap((pokemon) => pokemon.types)
+                        );
+                        setTypes(Array.from(allTypes));
+                        setLoading(false);
+
+                        // Aplica o filtro ao carregar a página se houver um tipo selecionado
+                        if (selectedType) {
+                            handleTypeFilter(selectedType, detailedPokemonData);
+                        }
+                    });
                 }
             });
-    }, []);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [selectedType]);
 
     useEffect(() => {
         if (!filteredPokemonData) return;
@@ -187,8 +76,8 @@ const Home = () => {
 
         observerRef.current = new IntersectionObserver(handleIntersection, {
             root: null,
-            rootMargin: '200px', // Carrega antes de entrar na visualização
-            threshold: 0.1, // Quando 10% estiver visível
+            rootMargin: '200px',
+            threshold: 0.1,
         });
 
         const cards = document.querySelectorAll('.card-placeholder');
@@ -201,42 +90,59 @@ const Home = () => {
         };
     }, [filteredPokemonData, visiblePokemon]);
 
-    const handleSearch = async (query) => {
+    const handleSearch = (query) => {
         if (!query) {
             setFilteredPokemonData(pokemonData);
-            setVisiblePokemon([]); // Reinicia os Pokémon visíveis
+            setVisiblePokemon([]);
             return;
         }
 
         const isNumeric = !isNaN(query);
         if (isNumeric) {
-            const pokemonById = pokemonData.find(pokemon => pokemon.id === parseInt(query));
+            const pokemonById = pokemonData.find((pokemon) => pokemon.id === parseInt(query));
             setFilteredPokemonData(pokemonById ? [pokemonById] : []);
         } else {
-            const pokemonByName = pokemonData.filter(pokemon =>
+            const pokemonByName = pokemonData.filter((pokemon) =>
                 pokemon.name.toLowerCase().includes(query.toLowerCase())
             );
-
-            if (pokemonByName.length === 0) {
-                try {
-                    const typeRes = await Axios.get(`${POKEMON_API_URL}/type/${query.toLowerCase()}`);
-                    const pokemonByType = typeRes.data.pokemon.map(p => p.pokemon.name);
-                    const pokemonByTypeData = pokemonData.filter(pokemon => pokemonByType.includes(pokemon.name));
-                    setFilteredPokemonData(pokemonByTypeData);
-                } catch (error) {
-                    console.log(error, "Tipo de Pokémon não encontrado");
-                    setFilteredPokemonData([]);
-                }
-            } else {
-                setFilteredPokemonData(pokemonByName);
-            }
+            setFilteredPokemonData(pokemonByName);
         }
-        setVisiblePokemon([]); // Reinicia os Pokémon visíveis após busca
+        setVisiblePokemon([]);
+    };
+
+    const handleTypeFilter = (type, data = pokemonData) => {
+        setSelectedType(type);
+        localStorage.setItem('selectedType', type); // Salva o tipo selecionado no localStorage
+
+        if (!type) {
+            setFilteredPokemonData(data);
+            return;
+        }
+
+        const filteredByType = data.filter((pokemon) => pokemon.types.includes(type));
+        setFilteredPokemonData(filteredByType);
     };
 
     return (
-        <main className='container'>
+        <main className="container">
+            <h3 className="home-title">PokéDex</h3>
             <SearchBar onSearch={handleSearch} />
+
+            <div className="filter-container">
+                <select
+                    id="type-filter"
+                    value={selectedType}
+                    onChange={(e) => handleTypeFilter(e.target.value)}
+                >
+                    <option value="">All types</option>
+                    {types.map((type) => (
+                        <option key={type} value={type}>
+                            {type.charAt(0).toUpperCase() + type.slice(1)}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
             <div className="cards--container">
                 {loading ? (
                     <Loading size={65} color={'var(--clr-dark-blue)'} />
